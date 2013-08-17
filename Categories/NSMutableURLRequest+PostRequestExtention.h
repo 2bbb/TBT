f932e06ed94e6452918dee8aa2d838f284df4f78
ISHII 2bit <ishii@buffer-renaiss.com>
2013-08-18 06:09:07 +0900

    begin develop post with file.

---
+++ Categories/NSMutableURLRequest+PostRequestExtention.h
@@ -0,0 +1,15 @@
+//
+//  NSMutableURLRequest+PostRequestExtention.h
+//  TBUtilitiesExample
+//
+//  Created by ISHII 2bit on 2013/08/18.
+//  Copyright (c) 2013年 buffer Renaiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableURLRequest (PostRequestExtention)
+
+
+
+@end
---
+++ Categories/NSMutableURLRequest+PostRequestExtention.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableURLRequest+PostRequestExtention.m
+//  TBUtilitiesExample
+//
+//  Created by ISHII 2bit on 2013/08/18.
+//  Copyright (c) 2013年 buffer Renaiss. All rights reserved.
+//
+
+#import "NSMutableURLRequest+PostRequestExtention.h"
+
+@implementation NSMutableURLRequest (PostRequestExtention)
+
+@end
