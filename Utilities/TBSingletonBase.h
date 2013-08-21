f7678967624ef8b7e5eb624b0ab6fdba78403c3e
ISHII Tsuubito <ishii@buffer-renaiss.com>
2013-08-22 00:35:45 +0900

    add Singleton Base Class

--- Utilities/TBSingletonBase.h
+++ Utilities/TBSingletonBase.h
@@ -1,13 +1,17 @@
 //
-//  TBSingletoneBase.h
+//  TBSingletonBase.h
 //  Gatsby
 //
-//  Created by ISHII Tsuubito on 2013/08/19.
-//  Copyright (c) 2013年 buffer Renaiss co., ltd. All rights reserved.
+//  Created by ISHII 2bit on 2013/08/19.
+//  Copyright (c) 2013 buffer Renaiss co., ltd. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface TBSingletoneBase : NSObject
+@interface TBSingletonBase : NSObject
+
++ (instancetype)sharedInstance;
+
+- (instancetype)initSharedInstance;
 
 @end
--- Utilities/TBSingletonBase.m
+++ Utilities/TBSingletonBase.m
@@ -1,13 +1,69 @@
 //
-//  TBSingletoneBase.m
+//  TBSingletonBase.m
 //  Gatsby
 //
-//  Created by ISHII Tsuubito on 2013/08/19.
-//  Copyright (c) 2013年 buffer Renaiss co., ltd. All rights reserved.
+//  Created by ISHII 2bit on 2013/08/19.
+//  Copyright (c) 2013 buffer Renaiss co., ltd. All rights reserved.
 //
 
-#import "TBSingletoneBase.h"
+#import "TBSingletonBase.h"
 
-@implementation TBSingletoneBase
+@implementation TBSingletonBase
+
+static NSMutableDictionary *_instances;
+
++ (instancetype)sharedInstance {
+    __block TBSingletonBase *singleton;
+    @synchronized(self) {
+        if ([_instances objectForKey:NSStringFromClass(self)] == nil) {
+            singleton = [[self alloc] initSharedInstance];
+        }
+    }
+    singleton = [_instances objectForKey:NSStringFromClass(self)];
+    return singleton;
+}
+
++ (id)allocWithZone:(NSZone *)zone {
+    @synchronized(self) {
+        if ([_instances objectForKey:NSStringFromClass(self)] == nil) {
+            id instance = [super allocWithZone:zone];
+            if (_instances == nil) {
+                _instances = [[NSMutableDictionary alloc] initWithCapacity:0];
+            }
+            [_instances setObject:instance forKey:NSStringFromClass(self)];
+            return instance;
+        }
+    }
+    return nil;
+}
+
+- (instancetype)initSharedInstance {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (id)init {
+    [self doesNotRecognizeSelector:_cmd];
+    return nil;
+}
+
+- (id)retain {
+    return self;
+}
+
+- (unsigned)retainCount {
+    return UINT_MAX;
+}
+
+- (oneway void)release {
+    // シングルトン状態を保持するため何もしない
+}
+
+- (id)autorelease {
+    return self;
+}
 
 @end
