d21645d521b95eb7ee2eee27af01db24183b864f
ISHII Tsuubito <ishii@buffer-renaiss.com>
2013-08-22 00:35:27 +0900

    add zoom extention

--- Categories/UIScrollView+ZoomingExtention.h
+++ Categories/UIScrollView+ZoomingExtention.h
@@ -1,13 +1,17 @@
 //
 //  UIScrollView+ZoomingExtention.h
-//  Townwork
 //
 //  Created by ISHII Tsuubito on 2013/08/06.
-//  Copyright (c) 2013年 buffer Renaiss co., ltd. All rights reserved.
+//  Copyright (c) 2013 buffer Renaiss co., ltd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface UIScrollView (ZoomingExtention)
+
+- (void)zoomToCenter:(float)scale withCenter:(CGPoint)center;
+- (void)zoomToCenter:(float)scale withCenter:(CGPoint)center animated:(BOOL)animated;
+- (void)zoomToPoint:(float)scale withPoint:(CGPoint)point;
+- (void)zoomToPoint:(float)scale withPoint:(CGPoint)point animated:(BOOL)animated;
 
 @end
--- Categories/UIScrollView+ZoomingExtention.m
+++ Categories/UIScrollView+ZoomingExtention.m
@@ -1,13 +1,50 @@
 //
 //  UIScrollView+ZoomingExtention.m
-//  Townwork
 //
 //  Created by ISHII Tsuubito on 2013/08/06.
-//  Copyright (c) 2013年 buffer Renaiss co., ltd. All rights reserved.
+//  Copyright (c) 2013 buffer Renaiss co., ltd. All rights reserved.
 //
 
 #import "UIScrollView+ZoomingExtention.h"
 
 @implementation UIScrollView (ZoomingExtention)
+
+- (void)zoomToCenter:(float)scale withCenter:(CGPoint)center {
+    [self zoomToCenter:scale withCenter:center animated:YES];
+}
+
+- (void)zoomToCenter:(float)scale withCenter:(CGPoint)center animated:(BOOL)animated {
+    CGRect zoomRect;
+    zoomRect.size.width  = self.frame.size.width  / scale;
+    zoomRect.size.height = self.frame.size.height / scale;
+    
+    zoomRect.origin.x = center.x - (zoomRect.size.width  / 2.0);
+    zoomRect.origin.y = center.y - (zoomRect.size.height / 2.0);
+    
+    //return zoomRect;
+    
+    [self zoomToRect:zoomRect animated:animated];
+    
+}
+
+- (void)zoomToPoint:(float)scale withPoint:(CGPoint)point {
+    [self zoomToPoint:scale withPoint:point animated:YES];
+}
+
+- (void)zoomToPoint:(float)scale withPoint:(CGPoint)point animated:(BOOL)animated {
+    CGRect zoomRect;
+    zoomRect.size.width  = self.frame.size.width  / scale;
+    zoomRect.size.height = self.frame.size.height / scale;
+    
+    CGFloat percentX = point.x / self.frame.size.width;
+    CGFloat percentY = point.y / self.frame.size.height;
+    
+    zoomRect.origin.x = point.x - percentX * zoomRect.size.width;
+    zoomRect.origin.y = point.y - percentY * zoomRect.size.height;
+    
+    //return zoomRect;
+    
+    [self zoomToRect:zoomRect animated:animated];
+}
 
 @end
