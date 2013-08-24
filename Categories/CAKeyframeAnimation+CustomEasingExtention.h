3dbdfa94696fe1518b00f8a64a9cc032fa150e9b
ISHII Tsuubito <ishii@buffer-renaiss.com>
2013-08-25 07:43:35 +0900

    add objectAtRandom

---
+++ Categories/CAKeyframeAnimation+CustomEasingExtention.h
@@ -0,0 +1,13 @@
+//
+//  CAKeyframeAnimation+ParametricExtention.h
+//  Gatsby
+//
+//  Created by ISHII Tsuubito on 2013/08/23.
+//  Copyright (c) 2013年 buffer Renaiss co., ltd. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CAKeyframeAnimation (ParametricExtention)
+
+@end
---
+++ Categories/CAKeyframeAnimation+CustomEasingExtention.m
@@ -0,0 +1,13 @@
+//
+//  CAKeyframeAnimation+ParametricExtention.m
+//  Gatsby
+//
+//  Created by ISHII Tsuubito on 2013/08/23.
+//  Copyright (c) 2013年 buffer Renaiss co., ltd. All rights reserved.
+//
+
+#import "CAKeyframeAnimation+ParametricExtention.h"
+
+@implementation CAKeyframeAnimation (ParametricExtention)
+
+@end
