fc4082bb262182fa78227818df52fe461c6ed134
ISHII Tsuubito <ishii@buffer-renaiss.com>
2013-08-26 08:40:41 +0900

    add _4inch.xib extention

--- Categories/UIViewController+RapidWriteExtention.m
+++ Categories/UIViewController+RapidWriteExtention.m
@@ -6,6 +6,7 @@
 //
 
 #import "UIViewController+RapidWriteExtention.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation UIViewController (RapidWriteExtention)
 
@@ -14,11 +15,63 @@
 }
 
 + (id)viewController {
-    return [self viewControllerWithNibName:NSStringFromClass([self class])];
+    NSString *nibName = NSStringFromClass([self class]);
+    if(500 < [[UIScreen mainScreen] applicationFrame].size.height) {
+        NSString *nibName4inch = [NSString stringWithFormat:@"%@_4inch", nibName];
+        if([[NSBundle mainBundle] pathForResource:nibName4inch ofType:@"nib"]) {
+            NSLog(@"%@ exists", nibName4inch);
+            nibName = nibName4inch;
+        } else {
+            NSLog(@"... %@ don't exists", nibName4inch);
+        }
+    }
+    return [self viewControllerWithNibName:nibName];
+}
+
+- (void)pushViewController:(UIViewController *)controller animated:(BOOL)animated {
+    [[self navigationController] pushViewController:controller animated:animated];
+}
+
+- (void)pushViewControllerWithDissolev:(UIViewController *)controller {
+    CATransition *transition = [CATransition animation];
+    transition.duration = 0.4;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionReveal;
+    transition.subtype = kCATransitionFromRight;
+    
+    [[[[self navigationController] view] layer] addAnimation:transition forKey:nil];
+    
+    [self pushViewController:controller animated:YES];
+}
+
+- (void)pushController:(Class)controllerClass animated:(BOOL)animated {
+    if([controllerClass isSubclassOfClass:[UIViewController class]]) {
+        UIViewController *controller = [controllerClass viewController];
+        [[self navigationController] pushViewController:controller animated:animated];
+    }
 }
 
 - (UIResponder *)appDelegate {
     return [[UIApplication sharedApplication] delegate];
 }
 
+- (void)popViewControllerAnimated:(BOOL)animated {
+    [[self navigationController] popViewControllerAnimated:animated];
+}
+
+- (IBAction)popViewController {
+    [self popViewControllerAnimated:YES];
+}
+
+- (void)popToRootViewControllerAnimated:(BOOL)animated {
+    [[self navigationController] popToRootViewControllerAnimated:YES];
+}
+
+- (IBAction)popToRootViewController {
+    [self popToRootViewControllerAnimated:YES];
+}
+
+- (IBAction)dismissViewController {
+    [self dismissModalViewControllerAnimated:YES];
+}
 @end
